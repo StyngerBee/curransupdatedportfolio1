1d01b56bbbe8f57fc91d611bae40ec52ab3b0253
Curran Duke <cdsonics@yahoo.com>
2023-01-14 00:43:38 -0500

    replaced placeholders and removed services section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,73 +1,38 @@
 import './portfolio.css'
 import ME from '../../assets/images/Placeholder.png'
+import Technigram from '../../assets/images/Technigram.PNG'
+import HL from '../../assets/images/HigherLower.PNG'
 
 const Portfolio = () => {
     return (
         <section id={"portfolio"}>
-            <h5>My Recent Work</h5>
-            <h2>Portfolio</h2>
+            
+            <h2>My Recent Work</h2>
 
             <div className={"container portfolio__container"}>
                 <article className={"portfolio__item"}>
                     <div className={"portfolio__item-image"}>
-                        <img src={ME} alt={''}/>
+                        <img src={Technigram} alt={''}/>
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Technigram</h3>
                     <div className={"portfolio__item-cta"}>
-                        <a href={"https://github.com"} className={'btn'} target={'_blank'}>Github</a>
-                        <a href={"https://dribbble.com"} className={'btn btn-primary'} target={"_blank"}>Live Demo</a>
+                        <a href={"https://github.com/PeteChicchetti/technigram"} className={'btn'} target={'_blank'}>Github</a>
+                        <a href={"https://murmuring-depths-50021.herokuapp.com"} className={'btn btn-primary'} target={"_blank"}>Demo</a>
                     </div>
                 </article>
                 <article className={"portfolio__item"}>
                     <div className={"portfolio__item-image"}>
-                        <img src={ME} alt={''}/>
+                        <img src={HL} alt={''}/>
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Higher or Lower: Artist Edition</h3>
                     <div className={"portfolio__item-cta"}>
-                        <a href={"https://github.com"} className={'btn'} target={'_blank'}>Github</a>
-                        <a href={"https://dribbble.com"} className={'btn btn-primary'} target={"_blank"}>Live Demo</a>
+                        <a href={"https://github.com/StyngerBee/higherlower"} className={'btn'} target={'_blank'}>Github</a>
+                        <a href={"https://styngerbee.github.io/higherlower/"} className={'btn btn-primary'} target={"_blank"}>Demo</a>
                     </div>
                 </article>
-                <article className={"portfolio__item"}>
-                    <div className={"portfolio__item-image"}>
-                        <img src={ME} alt={''}/>
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className={"portfolio__item-cta"}>
-                        <a href={"https://github.com"} className={'btn'} target={'_blank'}>Github</a>
-                        <a href={"https://dribbble.com"} className={'btn btn-primary'} target={"_blank"}>Live Demo</a>
-                    </div>
-                </article>
-                <article className={"portfolio__item"}>
-                    <div className={"portfolio__item-image"}>
-                        <img src={ME} alt={''}/>
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className={"portfolio__item-cta"}>
-                        <a href={"https://github.com"} className={'btn'} target={'_blank'}>Github</a>
-                        <a href={"https://dribbble.com"} className={'btn btn-primary'} target={"_blank"}>Live Demo</a>
-                    </div>
-                </article>
-                <article className={"portfolio__item"}>
-                    <div className={"portfolio__item-image"}>
-                        <img src={ME} alt={''}/>
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className={"portfolio__item-cta"}>
-                        <a href={"https://github.com"} className={'btn'} target={'_blank'}>Github</a>
-                        <a href={"https://dribbble.com"} className={'btn btn-primary'} target={"_blank"}>Live Demo</a>
-                    </div>
-                </article>
-                <article className={"portfolio__item"}>
-                    <div className={"portfolio__item-image"}>
-                        <img src={ME} alt={''}/>
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className={"portfolio__item-cta"}>
-                        <a href={"https://github.com"} className={'btn'} target={'_blank'}>Github</a>
-                        <a href={"https://dribbble.com"} className={'btn btn-primary'} target={"_blank"}>Live Demo</a>
-                    </div>
-                </article>
+                
+                
+                
 
             </div>
 
