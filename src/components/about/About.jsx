26fa573dc6f4978bf326e4ffd09369d1a82957f8
Curran Duke <cdsonics@yahoo.com>
2023-02-22 00:41:01 -0500

    deleted extra contact me button and reordered about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,22 +25,16 @@ const About = () => {
             </article>
             <article className='about__card'>
               <FaAward className='about__icon'/>
-              <h5>Discount Tire</h5>
-              <small>6 months working as a service technician, learning to use my hands in the workplace and getting proficient at working on cars.</small>
-            </article>
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
               <h5>Florida School for the Deaf and Blind</h5>
               <small>Over 2 years of working as a mentor, and an instructor for ages k-12, in the dorms and guiding the future of our society.</small>
             </article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Discount Tire</h5>
+              <small>6 months working as a service technician, learning to use my hands in the workplace and getting proficient at working on cars.</small>
+            </article>
+            
           </div>
-          {/* <p> 
-            I'm Curran Duke, a practicing software developer, who strives to learn more about programming, in order to excel in my career. I enjoy playing games at home, and working on my car and my motorcycle. 
-            I also enjoy problem solving and expanding my skill set in topics that I find interest in. 
-
-          </p> */}
-
-          <a href='#contact'>Contact Me</a>
         </div>
       </div>
     </section>
