f129cb67d845bbb58b18da181ae8d6da978a7807
Curran Duke <cdsonics@yahoo.com>
2023-02-22 00:37:25 -0500

    changed header to eliminate extra skills

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h1> Curran Duke</h1>
         <h3 className='text-light'> Fullstack Developer</h3>
-        <h5 className='text-light'> I am a practicing software developer, who strives to incorporate my existing developer knowledge, people skills and management skills to excel in my career and expand my skill sets.   I am seeking an opportunity in software development or related fields.  In my spare time I enjoy rebuilding cars and motorcycles where my problem solving skills are a plus.  I am a motivated and fast learner and recently graduated from UCF's Full Stack Coding Bootcamp.</h5>
+        <h5 className='text-light'> I am a practicing software developer, who strives to incorporate my existing developer knowledge, people and management skills to excel in my career and expand my programming knowledge.   I am seeking an opportunity in software development or related fields.  In my spare time I enjoy rebuilding cars and motorcycles where being a problem solver is a plus.  I am a motivated and fast learner and recently graduated from UCF's Full Stack Coding Bootcamp.</h5>
         <CTA></CTA>
         <HeaderSocials/>
         <div className='me'>
